1406d7a1df10544544e79ca42877670c3b33c180
bor <bo90ris@gmail.com>
2025-03-25 14:07:20 +0300

    Commit services

---
+++ Services.Abstractions/INotificationService.cs
@@ -0,0 +1,13 @@
+﻿using Services.Contracts.NotificationDto;
+
+namespace Services.Abstractions;
+
+public interface INotificationService
+{
+    /// <summary>
+    /// Получить произвольный ID уведомления
+    /// </summary>
+    /// <returns></returns>
+    Task<NotificationDefaultIdDto> GetDefaultIdAsync();
+    
+}
---
+++ Services.Implementations/NotificationService.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Services.Abstractions;
+using Services.Contracts.NotificationDto;
+using Services.Repositories;
+
+namespace Services.Implementations;
+
+public class NotificationService(
+    INotificationRepository service,
+    IMapper mapper) : INotificationService
+{
+    //private readonly INotificationRepository _notificationRepository;
+    private readonly INotificationRepository _service = service ?? throw new ArgumentNullException(nameof(service));
+    private readonly IMapper _mapper = mapper  ?? throw new ArgumentNullException(nameof(mapper));
+
+    /// <summary>
+    /// Получить произвольный ИД для уведомления
+    /// </summary>
+    /// <returns></returns>
+    public async Task<NotificationDefaultIdDto> GetDefaultIdAsync()
+    {
+        // var defaultId = await _notificationRepository.GetDefaultIdAsync();
+        var defaultId = await _service.GetDefaultIdAsync();
+        //return _mapper.Map<int, NotificationDefaultIdDto>(defaultId);
+        return _mapper.Map<NotificationDefaultIdDto>(defaultId);
+    }
+}

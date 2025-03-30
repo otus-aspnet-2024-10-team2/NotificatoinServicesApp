77a9ceae25feff087858136962985ab1482a81af
user <bo90ris@gmail.com>
2025-03-30 20:39:16 +0300

    Добавлен в инфру сборка под masstransit

---
+++ Infrastructure.Messaging/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Infrastructure.Messaging;
+
+public class Class1
+{
+}
---
+++ Infrastructure.Messaging/MessageContracts/NotificationFileMessage.cs
@@ -0,0 +1,6 @@
+namespace Infrastructure.Messaging.MessageContracts;
+
+public class NotificationFileMessage
+{
+    public string FilePath { get; set; }
+}

415c10e9cd8ca460c0e345f26b092917447e7abd
bor <bo90ris@gmail.com>
2025-04-04 13:19:05 +0300

    Добавлена DTO для выгрузки ряда уведомлений

---
+++ Services.Contracts/Notification/NotificationPageDto.cs
@@ -0,0 +1,23 @@
+﻿namespace Services.Contracts.Notification;
+
+public class NotificationPageDto
+{
+    /// <summary>
+    /// Уведомления
+    /// </summary>
+    public IEnumerable<NotificationDto> NotificationsList { get; set; }  
+    
+    /// <summary>
+    /// Дата, с которой смотреть уведомления
+    /// </summary>
+    public DateTime? BeginDate { get; set; }
+    /// <summary>
+    /// Дата, по которую смотреть уведомления
+    /// </summary>
+    public DateTime? EndDate { get; set; }
+    /// <summary>
+    /// Текущая дата
+    /// </summary>
+    public DateTime? CurrentDate { get; set; }
+    
+}

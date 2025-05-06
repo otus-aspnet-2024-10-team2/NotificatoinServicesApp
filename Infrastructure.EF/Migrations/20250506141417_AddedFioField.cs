c229b50f34991a6160858faf847ec838e87c717f
bor <bo90ris@gmail.com>
2025-05-06 17:23:09 +0300

    added migration files

---
+++ Infrastructure.EF/Migrations/20250506141417_AddedFioField.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedFioField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Notifications",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "Notifications",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Notifications",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Notifications");
+        }
+    }
+}
